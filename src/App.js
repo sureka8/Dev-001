aeb3ea190a90d5c6b032d938ac9eef2b201308dd
sureka8 <cst19048@std.uwu.ac.lk>
2024-04-30 11:47:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Agents from "./components/Agents";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Valuation from "./components/Valuation";
+import Villa from "./components/Villa";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import Specialdeal from "./pages/Specialdeal";
+import Store from "./pages/Store";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-display" >
+    
+    <Home />
+    <Villa />
+    <Store />
+    <Specialdeal />
+    <Agents />
+    <Clients />
+    <Valuation />
+    <Blog />
+   
+    <Footer />
     </div>
   );
 }
